93b19107f1500b1d31c82072f1b79919989567cc
b7w@isudo.ru <b7w@isudo.ru>
2018-12-12 19:13:24 +0300

    Housekeeping

--- src/main/kotlin/me/b7w/dbscale/PgDataLoader.kt
+++ src/main/kotlin/me/b7w/dbscale/PgDataLoader.kt
@@ -1,21 +1,27 @@
 package me.b7w.dbscale
 
-import io.vertx.core.json.JsonArray
 import io.vertx.ext.asyncsql.AsyncSQLClient
 import io.vertx.kotlin.core.json.JsonArray
-import io.vertx.kotlin.coroutines.awaitResult
+import io.vertx.kotlin.ext.sql.commitAwait
+import io.vertx.kotlin.ext.sql.getConnectionAwait
+import io.vertx.kotlin.ext.sql.querySingleWithParamsAwait
+import io.vertx.kotlin.ext.sql.setAutoCommitAwait
+import kotlinx.coroutines.coroutineScope
 import java.util.*
 import kotlin.random.Random
 
 class PgDataLoader(val client: AsyncSQLClient) {
 
-    suspend fun insertUsers(count: Long): String {
-        awaitResult<JsonArray> {
+    suspend fun insertUsers(count: Long): String = coroutineScope {
+        val connection = client.getConnectionAwait()
+        connection.setAutoCommitAwait(false)
+        for (i in 0..count) {
             val params = JsonArray(UUID.randomUUID(), Random.nextLong(999999999999999999L))
             LOG.trace("Insert params: {}", params)
-            client.querySingleWithParams("INSERT INTO users VALUES (?, ?)", params, it)
+            connection.querySingleWithParamsAwait("INSERT INTO users VALUES (?, ?)", params)
         }
-        return "Success $count"
+        connection.commitAwait()
+        return@coroutineScope "Success $count"
     }
 
 }
