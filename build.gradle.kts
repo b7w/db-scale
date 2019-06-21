a7fc4be3040430c121957737cc4655fccce7ae25
b7w@isudo.ru <b7w@isudo.ru>
2019-06-21 19:24:32 +0300

    Housekeeping

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.20"
+    kotlin("jvm") version "1.3.40"
 }
 
 group = "me.b7w"
 version = "1.0-SNAPSHOT"
 
-val kotlinVersion by extra("1.3.20")
-val vertxVersion by extra("3.6.2")
+val kotlinVersion by extra("1.3.40")
+val vertxVersion by extra("3.7.1")
 val junitVersion by extra("4.12")
 
 
@@ -20,7 +20,7 @@ dependencies {
     compile(kotlin("stdlib-jdk8"))
     compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
     compile("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
-    compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
+    compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
 
     compile("io.vertx:vertx-core:${vertxVersion}")
     compile("io.vertx:vertx-lang-kotlin:${vertxVersion}")
@@ -30,10 +30,9 @@ dependencies {
     compile("io.vertx:vertx-config:${vertxVersion}")
     compile("io.vertx:vertx-mongo-client:${vertxVersion}")
     compile("io.vertx:vertx-cassandra-client:${vertxVersion}")
+    compile("io.vertx:vertx-redis-client:${vertxVersion}")
 
-    compile("io.reactiverse:reactive-pg-client:0.11.2")
-    compile("org.msgpack:msgpack-core:0.8.16")
-    compile("org.msgpack:jackson-dataformat-msgpack:0.8.16")
+    compile("io.reactiverse:reactive-pg-client:0.11.4")
 
     compile("ch.qos.logback:logback-classic:1.2.3")
     compile("org.apache.logging.log4j:log4j-to-slf4j:2.11.1")
